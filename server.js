554272b25ab2548ee1a4fea87f369b7c2d6fbcc5
Michał Sakwa <saka1988@gmail.com>
2015-12-31 00:12:56 +0100

    Cleaning up sample app

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-#!/bin/env 
+#!/bin/env
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var GatherUp = function() {
-
-    //  Scope.
+var GatherUp = function () {
     var self = this;
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
+    self.setupVariables = function () {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        self.dbConnection = process.env.OPENSHIFT_MONGODB_DB_URL || 'localhost:27017';
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -25,104 +21,75 @@ var GatherUp = function() {
         };
     };
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
+    self.terminator = function (sig) {
         if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
+            console.log('%s: Received %s - terminating sample app ...',
+                Date(Date.now()), sig);
+            process.exit(1);
         }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
+        console.log('%s: Node server stopped.', Date(Date.now()));
     };
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
+    self.setupTerminationHandlers = function () {
         //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
+        process.on('exit', function () {
+            self.terminator();
+        });
 
         // Removed 'SIGPIPE' from the list - bugz 852598.
         ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
          'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+        ].forEach(function (element, index, array) {
+            process.on(element, function () {
+                self.terminator(element);
+            });
         });
     };
 
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'application/json');
-            res.send({'title':'GatherUp'});
-        };
-    };
-    
-    self.useRoutes = function(app) {
-        app.post('/testpost', function(req, res) {
-            console.log(req.body); 
+    self.useRoutes = function (app) {
+        //TODO REPLACE WITH SINGLE FILES
+        app.get('/', function (req, res) {
+            res.json({
+                'title': 'GatherUp'
+            });
+        });
+        
+        app.post('/testpost', function (req, res) {
+            console.log(req.body);
             res.send(200);
         });
     };
 
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
+    self.initializeServer = function () {
         self.app = express();
         self.app.use(bodyParser.json());
         self.app.use(bodyParser.urlencoded());
-        
-        self.useRoutes(self.app);
 
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
+        self.useRoutes(self.app);
     };
 
+    self.initDB = function () {
+        self.db = mongoose.connect(self.dbConnection);
+        console.log('Connected to mongo at address: ' + self.dbConnection);
+    }
 
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
+    self.initialize = function () {
         self.setupVariables();
         self.setupTerminationHandlers();
 
-        // Create the express server and routes.
         self.initializeServer();
+        self.initDB();
     };
 
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
+    self.start = function () {
         //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
+        self.app.listen(self.port, self.ipaddress, function () {
             console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+                Date(Date.now()), self.ipaddress, self.port);
         });
     };
 
-}; 
+};
 
 var gu = new GatherUp();
 gu.initialize();
 gu.start();
-
